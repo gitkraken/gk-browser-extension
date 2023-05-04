28024294688b7e95ef454f0160e577eb1e4f5013
Eric Amodio <eamodio@gmail.com>
2023-05-04 18:30:04 -0400

    Adds option to "Code" dropdown UI for repo/tree/PR Removes button from Repos (see above)

--- src/provider.ts
+++ src/provider.ts
@@ -1,11 +1,5 @@
 export type LinkTarget = 'gitkraken' | 'gkdev' | 'vscode' | 'vscode-insiders';
 
-export interface RenderConfig {
-	location: 'before' | 'after';
-	selector: string;
-	additionalClasses?: string[];
-}
-
 export interface InjectionProvider {
 	inject(): void;
 }
