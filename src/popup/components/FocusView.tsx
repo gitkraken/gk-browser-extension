25d7549a952a7cdf3ec00fc5a8bf8e637a6ed96d
Jose Garcia <jose.garcia@gitkraken.com>
2024-04-09 15:13:50 -0700

    Add styling to the Focus View

--- src/popup/components/FocusView.tsx
+++ src/popup/components/FocusView.tsx
@@ -1,17 +1,34 @@
-import type { PullRequestBucket } from '@gitkraken/provider-apis';
+import type { GitPullRequest, PullRequestBucket } from '@gitkraken/provider-apis';
 import { GitHub, GitProviderUtils } from '@gitkraken/provider-apis';
 import React, { useEffect, useState } from 'react';
 import { fetchProviderToken } from '../../gkApi';
 
+const PullRequestRow = ({ pullRequest }: { pullRequest: GitPullRequest }) => {
+	return (
+		<div className="pull-request">
+			<div className="pull-request-title-and-number">
+				<span className="pull-request-title truncate">{pullRequest.title}</span>{' '}
+				<a className="pull-request-number" href={pullRequest.url || undefined} target="_blank">
+					#{pullRequest.number}
+				</a>
+			</div>
+			<div className="repository-name truncate">{pullRequest.repository.name}</div>
+			<a href={pullRequest.url || undefined} target="_blank">
+				<i className="fa-regular fa-arrow-up-right-from-square icon" />
+			</a>
+		</div>
+	);
+};
+
 const Bucket = ({ bucket }: { bucket: PullRequestBucket }) => {
 	return (
-		<div>
-			<div>
-				<i className={`fa-regular fa-${bucket.faIconName}`} />
+		<div className="pull-request-bucket">
+			<div className="pull-request-bucket-header">
+				<i className={`fa-regular fa-${bucket.faIconName} icon`} />
 				{bucket.name}
 			</div>
 			{bucket.pullRequests.map(pullRequest => (
-				<div key={pullRequest.id}>{pullRequest.title}</div>
+				<PullRequestRow key={pullRequest.id} pullRequest={pullRequest} />
 			))}
 		</div>
 	);
@@ -52,7 +69,7 @@ export const FocusView = () => {
 	}, []);
 
 	return (
-		<div>
+		<div className={pullRequestBuckets.length ? 'focus-view' : ''}>
 			{loadingPullRequests && <i className="fa-regular fa-spinner-third fa-spin" />}
 			{pullRequestBuckets.map(bucket => (
 				<Bucket key={bucket.id} bucket={bucket} />
