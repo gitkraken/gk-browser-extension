4ead755458de0790b5e02e23ef69a2d4598ee9d9
Jose Garcia <jose.garcia@gitkraken.com>
2024-04-12 16:56:44 -0700

    Add images to Sign In UI

--- src/popup/components/SignedOut.tsx
+++ src/popup/components/SignedOut.tsx
@@ -7,6 +7,7 @@ export const SignedOut = () => {
 			<div className="sign-in-prompt">
 				<div className="text-2xl bold">Sign in to view your Pull Requests</div>
 				<a className="sign-in-link text-sm text-secondary bg-02" href={`${GKDotDevUrl}/login`} target="_blank">
+					<img src="img/gk-logo-36.svg" height={36} width={36} />
 					Sign in with GitKraken
 				</a>
 				<a className="text-link" href={`${GKDotDevUrl}/register`} target="_blank">
